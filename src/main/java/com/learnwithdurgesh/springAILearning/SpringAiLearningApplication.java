cd443f688bba84cc26cca4345e03f11ad35b84db
Gopal Patidar <rimjhim.gopal-patidar@capgemini.com>
2026-08-31 10:25:52 +0530

    first simple implementation of spring ai

---
+++ src/main/java/com/learnwithdurgesh/springAILearning/SpringAiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learnwithdurgesh.springAILearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnwithdurgesh/springAILearning/SpringAiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithdurgesh.springAILearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
